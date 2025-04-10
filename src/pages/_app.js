2ee97a3dc356982e07603a3114fd0624d8b18ccd
FReptar0 <20213tn137@utez.edu.mx>
2025-04-10 14:40:05 -0600

    Add global and index styles, and implement a light theme
    - Created global CSS file for base styles and layout adjustments.
    - Added index CSS file with custom styles for search bar and responsive design.
    - Implemented a light theme using Material-UI's createTheme function.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,28 @@
-import "@/styles/globals.css";
+// pages/_app.js
+import "@/styles/global.css";
+import "@/styles/index.css";
+import Head from "next/head";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import darkTheme from "../utils/theme";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/logos/logo.png" />
+        <link href="/logos/logo.png" rel="shortcut icon" type="image/x-icon" />
+        <title>Qardeal</title>
+      </Head>
+
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
+
+
+export default MyApp;
